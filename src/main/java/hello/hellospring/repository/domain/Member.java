473c4a5b367b512a9b4fb719e9f0ff119d9f804e
hunojung <huno0504@naver.com>
2022-06-21 23:24:17 +0900

    inflean ê°lecture 'spring basic db access techniques' complete

---
+++ src/main/java/hello/hellospring/repository/domain/Member.java
@@ -0,0 +1,51 @@
+package hello.hellospring.repository.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+/*
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+*/
